9824dbf33f53b9049656db30c29c0d1f540365fd
Charlotte Seve <charlotte.m.seve@gmail.com>
2023-11-23 14:44:28 +0100

    setup working

--- make.R
+++ make.R
@@ -1,6 +1,6 @@
 
 
-# Projetc setup ----
+# Project setup ----
 
 # Create a description file
 ## NB: check_name = FALSE because we are not creating a package
@@ -16,8 +16,21 @@
 # Use renv with DESCRIPTION file (explicit use of renv)
 # renv::init()
 
+
+# debug renv on windows
+# options(renv.download.override = utils::download.file)
+
 # Create R functions in the R repository
 # usethis::use_r("load data")
+
+
+# Development commands----
+
+# Load packages from DESCRIPTION file
+# renv::install() 
+# renv::snapshot(prompt = FALSE)
+
+
 
 # workflow ----
 
