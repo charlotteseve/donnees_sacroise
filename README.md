8ad87c505d5c6db5a5cbb4e560059bdb2ea949ed
Charlotte Seve <charlotte.m.seve@gmail.com>
2023-11-23 14:15:31 +0100

    create structure

---
+++ README.md
@@ -0,0 +1,26 @@
+
+# DB_peche
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of DB_peche is to ...
+
+## Installation
+
+You can install the development version of DB_peche from [GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("charlotteseve/donnees_sacroise")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(DB_peche)
+## basic example code
+```
+
---
+++ make.R
@@ -0,0 +1,28 @@
+
+
+# Projetc setup ----
+
+# Create a description file
+## NB: check_name = FALSE because we are not creating a package
+# usethis::use_description(check_name = FALSE) 
+
+# Create a readme file
+# usethis::use_readme_md()
+
+# Create compendium folders
+# lapply(c("data", "R", "analyses", "outputs", "data/notebook", "data/sacrois"), # elements I want to apply the function to
+#       dir.create, showWarnings = FALSE) # the function
+
+# Use renv with DESCRIPTION file (explicit use of renv)
+# renv::init()
+
+# Create R functions in the R repository
+# usethis::use_r("load data")
+
+# workflow ----
+
+
+# Titre projet
+# Description projet
+
+
