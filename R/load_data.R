3744ccf03e640d7b10fef66062759d89bb07d8b6
MaelGernez <mael.gernez@ifremer.fr>
2023-11-23 15:57:31 +0100

    finir load_data

--- R/load_data.R
+++ R/load_data.R
@@ -8,5 +8,10 @@ load_data <- function(){
   
   ports_long <-read.csv2('data/sacrois/ports_long.csv')
   
+  return(c("data/sacrois/SACROIS_obsDeb_Sars_2018_2022_GSA07.csv",
+           'data/sacrois/SACROIS_obsDeb_effort_Sars_2018_2022_GSA07.csv',
+           'data/sacrois/ANNEE_TRIM_TEMPS.csv',
+           'data/sacrois/ports_long.csv'))
+  
   
 }
