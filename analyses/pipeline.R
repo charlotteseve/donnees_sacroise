e2e7f174fc552a3ebc404f407d4652a6a27edd2b
Charlotte Seve <charlotte.m.seve@gmail.com>
2023-11-23 16:29:47 +0100

    create pipeline and update makeR

---
+++ analyses/pipeline.R
@@ -0,0 +1,20 @@
+# Load packages
+library(targets)
+library(tarchetypes)
+
+# Load functions in R file
+tar_source()
+
+# Creating pipeline
+list(
+  
+  # download data
+  
+  
+  # change encoding
+  
+  
+  
+  
+  
+)
